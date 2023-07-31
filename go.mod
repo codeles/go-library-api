b1b85cef373052be95f135d935cbcaa3378cdff3
Les Martin <martin.les@gmail.com>
2023-07-30 19:07:05 -0500

    introducing jwt to the project (WIP)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module codeles/library
 
 go 1.20
+
+require github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
--- main.go
+++ main.go
@@ -15,11 +15,7 @@ func fooHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("Opening the library...")
 
-	http.HandleFunc("/books", controllers.BookRouter)
-
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	http.HandleFunc("/books/", controllers.BookRouter)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
